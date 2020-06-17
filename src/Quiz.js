1dec8773512f4da0109ec3fb2db6e4087281d8b0
Louie <louie.gabriel@gmail.com>
2020-06-17 14:26:28 +0100

    hi

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,12 +8,16 @@ class Quiz extends Component {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length); 
         return <div>
             {isQuizEnd ? <QuizEnd /> :
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} instruction_text={quizData.instruction_text}/> }
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} instruction_text={quizData.instruction_text} showNextQuestionHandler={this.showNextQuestion.bind(this)}/> }
             </div>
     }
     constructor(props) {
         super(props)
         this.state = {quiz_position: 1}
+    }
+    showNextQuestion() {
+        this.setState({quiz_position: this.state.quiz_position + 1})
+        console.log(this.state.quiz_position)
     }
 }
 
